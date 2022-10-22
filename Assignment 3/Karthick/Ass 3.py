3fed971460f5799dd275e39b4b9ee95563b7598b
Karthick <113667560+k2001arthick@users.noreply.github.com>
2022-10-22 09:06:21 +0530

    Update Ass 3.py

--- Assignment 3/Karthick/Ass 3.py
+++ Assignment 3/Karthick/Ass 3.py
@@ -1,3 +1,4 @@
+                                            Traffic Light                                                                    
 from gpiozero import LED
 from time import sleep
 
@@ -28,3 +29,19 @@ while True:
         print("Remaining time: ",i)
         sleep(1)
     green.off()
+                                                  Blink LED         
+        
+        
+import RPi.GPIO as GP
+from time import sleep
+GP.setwarnings(False)
+GP.setmode(GP.BOARD)
+GP.setup(8,GP.OUT,initial=GP.LOW)                                                             
+while True:                      #infinite loop
+    GP.output(8, GPIO.HIGH)               # Turn on
+    print("The LED is ON")
+    sleep(2)                     # Sleep for 2 second
+    
+    GP.output(8, GPIO.LOW)                # Turn off
+    print("The LED is OFF")
+    sleep(2)                     # Sleep for 2 second
